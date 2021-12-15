861b451e6aa4f023fc1cc749876e832ad02ef3c3
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2021-12-15 09:42:39 -0500

    The functional Process for Login
    - Corrections for activation Page.
    - Registration and Login of Users with Central Medical Option.
    - Heroku deploy for backed or api site
    
    #2021215a

---
+++ my-app/src/pages/token.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { REACT_APP_API_URL } from '../utils/variables.js';
+
+export class Token extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      token: "",
+      message: localStorage.getItem("message"),
+    }
+    this.Token = "";
+    this.site = "token";
+  }
+
+  updateUser() {
+    console.log(REACT_APP_API_URL + this.site + '/activate')
+    fetch(REACT_APP_API_URL + this.site + '/activate', {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'x-auth-token': this.Token
+      },
+      body: JSON.stringify({
+        token: this.state.Token
+      })
+    })
+      .then(res => res.json())
+      .then((result) => {
+        if (!result.message) {
+          console.log(result.error);
+          alert(result.error)
+        } else {
+          localStorage.setItem("message", result.message);
+          this.setState({ message: result.message });
+          setTimeout(console.log('href', window.location.href), 2000);
+          let spl = window.location.href.split('?');
+          window.location.replace(spl[0]);
+        }
+      }, (error) => {
+        alert('Failed', error);
+      });
+  }
+
+  getParams() {
+    let params = {};
+    window.location.search.slice(1).split('&').forEach(elm => {
+      if (elm === '') return;
+      let spl = elm.split('=');
+      const d = decodeURIComponent;
+      params[d(spl[0])] = (spl.length >= 2 ? d(spl[1]) : true);
+    });
+    if (!params.value || !params.Token) {
+      localStorage.removeItem("message");
+    } else {
+      // eslint-disable-next-line react/no-direct-mutation-state
+      this.state.Token = params.value;
+      this.Token = params.Token;
+      //Call the: post(), put(), delete(), options(), trace(), copy(), lock(), mkcol(), 
+      //move(), purge(), propfind(), proppatch(), unlock(), report(), mkactivity(), checkout(), 
+      //merge(), m-search() , notify(), subscribe(), unsubscribe(), patch(), search(), and connect().
+      this.updateUser()
+    }
+  }
+
+  componentDidMount() {
+    this.getParams();
+  }
+
+  render() {
+    const {
+      message
+    } = this.state;
+    return (
+      <div>
+        <h3 className="d-flex justify-content-center m-3">
+          {message}
+        </h3>
+      </div>
+    )
+  }
+}
---
+++ my-app/src/utils/variables.js
@@ -0,0 +1,5 @@
+import {} from 'dotenv/config';
+export const {
+  REACT_APP_API_URL,
+  REACT_APP_PHOTO_URL,
+} = process.env
