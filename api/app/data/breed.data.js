c7fef0d43dfc29a51e7ceb146d4693f26f7d011f
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2023-08-17 17:27:48 -0500

    feat: Using in Init_All() just the 'sequelize'
    - Change for all init_x() to use the sequeliza.x.bulkCreate().
    - Transform the *.CSV file in a *.JSON file.
    - 'npm uninstall csvtojson', to remove this library.
    
    #20230817a

--- api/app/data/breed.data.js
+++ api/app/data/breed.data.js
@@ -1,35 +1,23 @@
-require('dotenv').config(); // import config = require('config');
-const csvtojson = require('csvtojson');
-const mysqlConnection = require('../utils/database.js');
-const setLog = require('../utils/logs.utils.js');
+const fs = require("fs");
+const db = require("../models");
+const setLog = require("../utils/logs.utils.js");
 
-function init_Breed() {
-  const fileName = process.cwd() + '/app/data/breed.data.csv';
-  const insertStatement =
-    `INSERT INTO ${process.env.MYSQL_D_B_}.Breeds
-        (breedId,SpeciesSpeciesId,breedName,createdAt,updatedAt)
-        VALUE (?,?,?,NOW(),NOW())`;
-
-  csvtojson().fromFile(fileName).then(source => {
-    for (var i = 0; i < source.length; i++) {
-      var
-        breedId = source[i]['breedId'];
-        SpeciesSpeciesId = source[i]['SpeciesSpeciesId'],
-        breedName = source[i]['breedName'];
-      if (!SpeciesSpeciesId || !breedName) { }
-      else {
-        var items = [breedId,SpeciesSpeciesId, breedName];
-        mysqlConnection.query(insertStatement, items,
-          (err, results, fields) => {
-            if (err) {
-              setLog("ERROR",__filename,arguments.callee.name,`'breedName', ${breedName}, 'SpeciesSpeciesId', ${SpeciesSpeciesId}`);
-              setLog("ERROR",__filename,arguments.callee.name,`Unable to insert item at row , ${i + 1}, ${err}`);
-            }
-          });
-      }
-    }
-    setLog("INFO",__filename,arguments.callee.name,`${i} record(s) for "Breeds" table was added successfully`);
+async function init_Breed (firstTry) {
+  const fileName = process.cwd() + "/app/data/breed.data.json";
+  const funcName = arguments.callee.name;
+  fs.readFile(fileName, function (err, data) {
+    setLog( "TRACE", __filename, funcName, `filename:${fileName}, firstTry:${firstTry}`);
+    db.breed.bulkCreate(JSON.parse(data.toString()),{ validate: true })
+      .then(() => {
+        setLog( "INFO", __filename, funcName, `${JSON.parse(data.toString()).length} record(s) for "Breeds" table was added successfully` );
+      })
+      .catch((err) => {
+        setLog( "ERROR", __filename, funcName, `Unable to insert items: ${err}` );
+        if (firstTry===true) setTimeout(() =>{ init_Breed(false); },2000);
+      })
+      .finally(() => {});
   });
+
 }
 
 module.exports = init_Breed;
--- api/app/data/species.data.js
+++ api/app/data/species.data.js
@@ -1,33 +1,20 @@
-require('dotenv').config(); // import config = require('config');
-const csvtojson = require('csvtojson');
-const mysqlConnection = require('../utils/database.js');
+const fs = require("fs");
+const db = require("../models");
 const setLog = require('../utils/logs.utils.js');
 
-function init_Species() {
-  const fileName = process.cwd() + '/app/data/species.data.csv';
-  const insertStatement =
-    `INSERT INTO ${process.env.MYSQL_D_B_}.Species
-        (speciesId,speciesName,speciesOrder,createdAt,updatedAt)
-        VALUE (?,?,?,NOW(),NOW())`;
-
-  csvtojson().fromFile(fileName).then(source => {
-    for (var i = 0; i < source.length; i++) {
-      var
-        speciesId = source[i]['speciesId'],
-        speciesName = source[i]['speciesName'];
-        speciesOrder = source[i]['speciesOrder'];
-      if (!speciesId || !speciesName) { }
-      else {
-        var items = [speciesId, speciesName, speciesOrder];
-        mysqlConnection.query(insertStatement, items,
-          (err, results, fields) => {
-            if (err) {
-              selLog("ERROR",__filename,arguments.callee.name,`'Unable to insert item at row ', ${i + 1}, ${err}`);
-            }
-          });
-      }
-    }
-    setLog("INFO",__filename,arguments.callee.name,`${i} record(s) for "Species" table was added successfully`);
+async function init_Species() {
+  const fileName = process.cwd() + "/app/data/species.data.json";
+  const funcName = arguments.callee.name;
+  fs.readFile(fileName, function (err, data) {
+    setLog( "TRACE", __filename, funcName, `filename:${fileName}`);
+    db.species.bulkCreate(JSON.parse(data.toString()),{ validate: true })
+      .then(() => {
+        setLog( "INFO", __filename, funcName, `${JSON.parse(data.toString()).length} record(s) for "Species" table was added successfully` );
+      })
+      .catch((err) => {
+        setLog( "ERROR", __filename, funcName, `Unable to insert items: ${err}` );
+      })
+      .finally(() => {});
   });
 }
 
